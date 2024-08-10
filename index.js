de2ca30f346b715727e4be2176552a06ff1c0c88
matheuscunha20 <matheu20pereira15@gmail.com>
2024-08-09 23:13:55 -0300

    meu ssegundo

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("ola mundo")
+// Variável para armazenar o nome e a quantidade de XP do herói
+let nomeHeroi = "Marco Romano";
+let xpHeroi = 6500;
+let nivel;
+
+// Estrutura de decisão para determinar o nível do herói
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else if (xpHeroi >= 10001) {
+    nivel = "Radiante";
+}
+
+// Saída da mensagem
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
